7e74c9a1ff414b333b59aef4436598fd13b016ec
DarkRuby <github@darkruby.co.uk>
2012-11-07 22:45:46 +0000

    init commit

---
+++ js/veridian.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $('.menu-item.inactive a').hover(
+        function()
+        {
+            $(this).animate({fontSize: '22px'}, 100);
+            if($(this).parent().hasClass('two-lines')) $(this).parent().animate({marginTop: '-8px'}, 100);
+            else $(this).parent().animate({marginTop: '-3px'}, 100);
+        },
+        function()
+        {
+            $(this).animate({fontSize: '14px'}, 100);
+            $(this).parent().animate({marginTop: '0px'}, 100);
+        }
+    );
+
+    Cufon.replace("h1", { fontFamily: 'Colaborate-Regular' } );
+    Cufon.replace("h2", { fontFamily: 'Colaborate-Regular' } );
+    Cufon.replace(".menu-item.active a", { fontFamily: 'Colaborate-Regular' } );
+
+
+    $('.scroll-pane').jScrollPane(
+    {
+        showArrows:false,
+        scrollbarWidth: 8,
+        scrollbarMargin: 40,
+        scrollbarOnLeft: true
+    });
+});
